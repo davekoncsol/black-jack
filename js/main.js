ef00b12fb21939c44c935afa5492a6bfb7dedc57
David Koncsol <davidkoncsol@Davids-MacBook-Pro.local>
2019-08-06 17:07:56 -0700

    added some functions including betting. Updated the board to render in a loop but this produced a bug. Will fix tomorrow

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ function createDeck(){
         }
     }
     
-    newdeck.push(deck);
+    newdeck = deck
 };
 
 
@@ -48,6 +48,9 @@ function createDeck(){
 
 let scores, money, bet, winner;
 
+let playerHand = [];
+
+let dealerHand = [];
 
 // let deck = [];
 
@@ -69,10 +72,9 @@ const scoreEl = {
 }
 
 const cardEl = {
-    pcard1: document.getElementById('pcard1'),
-    pcard2: document.getElementById('pcard2'),
-    dcard1: document.getElementById('dcard1'),
-    dcard2: document.getElementById('dcard2')
+    player: document.getElementById('player-box'),
+    dealer: document.getElementById('dealer-box'),
+  
 }
 
 const betEl = document.getElementById('bet');
@@ -109,21 +111,97 @@ init();
 function init() {
     money = 100;
     bet = 0;
+    winner = null;
+    scores = {
+        player: 0,
+        dealer: 0
+    }
+    playerCards = {
+        card1: 'back-blue',
+        card2: 'back-blue',
+        
+    }
+
+    dealerCards = {
+        card1: 'back-red',
+        card2: 'back-red',
+    }
+
+  
 
     render();
 
+
 }
+
+function getPlayerScore(){
+    for (let card in playerCards){
+        if (playerCards[card].length >= 3){
+            scores.player += parseInt(playerCards[card].slice(1));
+        } else if (playerCards[card].length < 3 && !playerCards[card].includes('A')){
+            scores.player += 10;
+        } else if (playerCards[card].includes('A')) {
+            scores.player += 11;
+
+        }
+        ;
+    }
+};
+function getDealerScore(){
+    for (let card in dealerCards){
+        if (dealerCards[card].length >= 3){
+            scores.dealer += parseInt(dealerCards[card].slice(1));
+        } else if (dealerCards[card].length < 3 && !dealerCards[card].includes('A')){
+            scores.dealer += 10;
+        } else if (dealerCards[card].includes('A')) {
+            scores.dealer += 11;
+
+        }
+        ;
+    }
+};
+
 
 //render
 //shows the results as well as the bet and money in the bank
 function render(){
     moneyEl.textContent = `Money = $${money}`;
     betEl.textContent = `Bet = $${bet}`;
+
+    
+    for (let pcard in playerCards){
+       console.log( playerCards[pcard]);
+       cardEl.player.innerHTML += `<img class="card shadow ${playerCards[pcard]}"> 
+       `
+    }
+    for (let dcard in dealerCards){
+       console.log( dealerCards[dcard]);
+       cardEl.dealer.innerHTML += `<img class="card shadow ${dealerCards[dcard]}"> 
+       `
+    }
+
+    scoreEl.dealer.textContent =`Dealer Number ${scores.dealer}`
+    scoreEl.player.textContent =`Player Number ${scores.player}`
+    // let templateDealer = ` <div class="card shadow ${dealerCards.card1}" id="dcard1"><img></div> 
+    // <div class="card shadow ${dealerCards.card2}" id="dcard2"><img></div> 
+    // <div id="dscore">Dealer Number ${scores.dealer} </div>
+    // `;
+
+    // let templatePlayer = `<div id='pscore'>Player Number ${scores.player}</div>
+    // <div class="card shadow ${playerCards.card1}" id="pcard1"><img></div> 
+    // <div class="card shadow ${playerCards.card2}" id="pcard2"><img></div> 
+    // `;
+    // cardEl.player.innerHTML = templatePlayer;
+    // cardEl.dealer.innerHTML = templateDealer;
+
+
 };
 
 //winner
 //identifies winner is dealer or player
 //if dealer wins, players loses money, if player wins, player wins money
+
+
 
 //start
 //the bet is set let the game BEGIN!
@@ -132,34 +210,120 @@ function render(){
 //bet
 //allows user to place bet
 function bet5() {
-    bet
-
-    
-
-};
+    console.log('bet 5!!')
+    if (money >= 5){
+    bet += 5;
+    money -= 5;
+    render();}
+     return;
+    };
 function bet10() {
     console.log('bet 10 clicked')
-    
+    if (money >= 10){
+        bet += 10;
+        money -= 10;
+        render();}
+         return;
+        };
 
-};
+
 function bet20() {
     console.log('bet 20 clicked')
+    if (money >= 20){
+        bet += 20;
+        money -= 20;
+        render();}
+         return;
+        };
+
+
+function chickenDinner(){
+    money += bet * 2;
+    bet = 0;
+    render ();
     
+}
 
-};
+function loser() {
+    bet = 0;
+    render();
+}
 
+function dealerAction(){
+
+}
 
 //player hit/stay
 
 function stay(){
-    console.log('stay');
+    console.log('stay then the dealer goes. ');
 }
 function hit(){
-    console.log('hit');
+    
+    
+    
+    getPlayerScore();
+render();
 }
 function start(){
-    console.log('start');
+    
+    shuffle(newdeck);
+    if(playerHand.length < 10){
+    for (i = 0; i <= 25; i++) {
+        // console.log(newdeck[i]);
+        playerHand.push(newdeck[i]);
+        
+    }
+    for (i = 26; i <=51; i++){
+        dealerHand.push(newdeck[i]);
+    }
 }
+    else { return}
+        
+        dealCards();
+        
+    }
+
+
+
+ 
+function shuffle(){
+    newdeck.sort( () => Math.random() - 0.5);
+    
+}
+
+//for loops for innerHTML?
+
+
+function dealCards(){
+    
+
+    playerCards.card1 = playerHand[0].suit + playerHand[0].value;
+    playerCards.card2 = playerHand[1].suit + playerHand[1].value;
+    dealerCards.card1 = dealerHand[0].suit + dealerHand[0].value;
+    dealerCards.card2 = dealerHand[1].suit + dealerHand[1].value;
+
+    
+getDealerScore();
+getPlayerScore();
+
+    render();
+}
+
+
+
+//for each 
+// function getValues(obj, objName){
+//     var result = '';
+//     for (var i in obj) {
+//         if (obj.hasOwnProperty(i)){
+//             result += `${objName}.${obj[i]}\n`
+//         }
+//     }
+//     console.log(result)
+// return result;
+
+// }
 
 
 
